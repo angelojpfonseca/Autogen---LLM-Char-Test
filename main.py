f01d874ca79b5ed5f1086b3099b89ebfe06e3d6a
angelojpfonseca <84818637+angelojpfonseca@users.noreply.github.com>
2024-06-19 00:27:11 +0200

    TEstes 02

--- main.py
+++ main.py
@@ -3,8 +3,24 @@ from dotenv import load_dotenv
 import openai
 from autogen import ConversableAgent, UserProxyAgent, GroupChatManager, GroupChat
 
+# Define the path to the system message file for agent1
+dm_system_message_path = r'D:\Git Hub\Autogen---Combat\Prompt\manager.txt'
+agent1_system_message_path = r'D:\Git Hub\Autogen---Combat\Prompt\agent1.txt'
+agent2_system_message_path = r'D:\Git Hub\Autogen---Combat\Prompt\agent2.txt'
+chat_manager_system_message_path = r'D:\Git Hub\Autogen---Combat\Prompt\Chat_Manager.txt'
 
+# Open the file and read its contents into a string variable
+with open(dm_system_message_path, 'r') as file:
+    manager_system_message = file.read()
 
+with open(agent1_system_message_path, 'r') as file:
+    agent1_system_message = file.read()
+
+with open(agent2_system_message_path, 'r') as file:
+    agent2_system_message = file.read()
+
+with open(chat_manager_system_message_path, 'r') as file:
+    chat_manager_system_message = file.read()
 
 load_dotenv()
 
@@ -15,46 +31,39 @@ llm_config = {
     "model": "gpt-4",
     "temperature": 0,
     "max_tokens": 256,
-    "cache_seed": 42,
+    "cache_seed": 50,
     "api_key": OPENAI_API_KEY
     }
 
-dm_combat = ConversableAgent(
-    name="Dungeon_Master",
-    system_message='DM_Prompt.txt',
+manager= ConversableAgent(
+    name="manager",
+    system_message=manager_system_message,
     llm_config=llm_config,
-    code_execution_config=False,
     human_input_mode="NEVER",
-        is_termination_msg=lambda msg: "terminate" in msg.get("content").lower(),
-
-)
+   )
 
 
 
 agent1 = ConversableAgent(
     name="Agent 1",
-    system_message= 'agent1.txt',
+    system_message= agent1_system_message,
     llm_config=llm_config,
-    code_execution_config=False,
     human_input_mode="NEVER",
-    is_termination_msg=lambda msg: "terminate" in msg.get("content").lower(),
-)
+    )
 
 agent2= ConversableAgent(
     name="Agent 2",
-    system_message='agent2.txt',
+    system_message=agent2_system_message,
     llm_config=llm_config,
-    code_execution_config=False,
     human_input_mode="NEVER",
-    is_termination_msg=lambda msg: "terminate" in msg.get("content").lower()
-)
+    )
 
 
 
 
 
 group_chat = GroupChat(
-    agents=[dm_combat, agent1, agent2],
+    agents=[manager, agent1, agent2],
     messages=[],
     max_round= 10,
 )
@@ -63,12 +72,12 @@ group_chat_manager = GroupChatManager(
     groupchat=group_chat,
     llm_config={"config_list": [{"model": "gpt-4", "api_key": OPENAI_API_KEY}]},
     human_input_mode="ALWAYS",
-    system_message='Chat_Manager.txt'
+    system_message=chat_manager_system_message
 )
 
 chat_result = group_chat_manager.initiate_chat(
-    dm_combat,
-    message="""Welcome to the game! Keep the game going as you do""",
+    manager,
+    message="""now you guys play Jo Ken Po!""",
     summary_method="reflection_with_llm",
 )
 
